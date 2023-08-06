21d9d93e957d24ba829f758209bdac6af602e731
Dimitur Stoqnov <mitko9711@gmail.com>
2023-08-06 14:24:18 +0300

    My Site

--- final_project/settings.py
+++ final_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8g%*bqfrl!-hov6l&u($5z1=(048(t1uyv!a!cr8gyi$yy+oca'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost:8000']
 
